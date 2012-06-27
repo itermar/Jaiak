1d0b14cb52e093fc2047b6ae4515986e52e1c0a1
Imanol Teran Maruri <itermar@gmail.com>
2012-06-27 13:15:06 +0200

    Sopelako Jaiak 2012

---
+++ Sopelana/app/app.js
@@ -0,0 +1,16 @@
+var App = (function(lng, undefined) {
+
+    //Define your LungoJS Application Instance
+    
+    lng.App.init({
+        name: 'Sopelako Jaiak 2012',
+        version: '1.0',
+        author: 'WIDDIAPPS SL'
+    });
+    
+
+    return {
+
+    };
+
+})(LUNGO);
