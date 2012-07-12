a2511b952ad5ac07d4e621cc75af7b9ed35953dc
Imanol Teran <itermar@gmail.com>
2012-07-12 22:06:15 +0200

    Solución visualización aside en tablet

--- Barakaldo/app/events.js
+++ Barakaldo/app/events.js
@@ -1,5 +1,11 @@
 App.Events = (function(lng, app, undefined) {
 
+    lng.ready (function()
+    {
+    	if (document.body.clientWidth > 481) //para el iPad
+    		lng.View.Aside.show('#main','#calendario');
+    });
+
     return {
 
     }
