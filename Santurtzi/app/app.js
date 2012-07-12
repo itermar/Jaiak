b70b32066060d5fa999363cea5a1863eaa01686a
Imanol Teran <itermar@gmail.com>
2012-07-12 18:17:31 +0200

    Minor changes

--- Santurtzi/app/app.js
+++ Santurtzi/app/app.js
@@ -3,14 +3,9 @@ var App = (function(lng, undefined) {
     //Define your LungoJS Application Instance
     
     lng.App.init({
-        name: 'Barakaldoko Jaiak 2012',
+        name: 'Santurtziko Jaiak 2012',
         version: '1.0',
         author: 'WIDDIAPPS SL',
-        resources: {
-            sections: [
-                'calendario.html'
-                ]
-        }
     });
     
 
