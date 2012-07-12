719f9892f2c3ecade4f29803f86e6107a7fb6afc
Imanol Teran <itermar@gmail.com>
2012-07-12 18:15:23 +0200

    Aplicación para smartphone y tablet con el programa de fiestas de Santurtzi

---
+++ Santurtzi/app/app.js
@@ -0,0 +1,21 @@
+var App = (function(lng, undefined) {
+
+    //Define your LungoJS Application Instance
+    
+    lng.App.init({
+        name: 'Barakaldoko Jaiak 2012',
+        version: '1.0',
+        author: 'WIDDIAPPS SL',
+        resources: {
+            sections: [
+                'calendario.html'
+                ]
+        }
+    });
+    
+
+    return {
+
+    };
+
+})(LUNGO);
